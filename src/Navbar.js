38a994fc57dba50b7a52c4206bd6e798761eee63
Belen <89939871+bmagdalenaa@users.noreply.github.com>
2023-01-23 08:54:29 -0600

    Add files via upload
    Navbar.js uploaded

---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+// components are functions that returns a jsx template and then that function is exported at the bottom of the file.
+import {Link} from 'react-router-dom';
+
+// using a snippet to create a functional component  - use sfc + tab to create a stateless functional component
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <div className="links">
+                 {/* For styling directly on file 
+                 <a href="/create" style={{
+                    // First set of curly brackets are a dynamic value and second is the js object
+                    // double curly brackets to repreneset key-value pair
+                    color: "white",
+                    backgroundColor: '#f1356d',
+                    borderRadius:'8px'
+                 }}>New Blog</a> */}
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
+            </div>
+        </nav> 
+    );
+}
+ 
+export default Navbar;
